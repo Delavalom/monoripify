56d1b444287f73c2d9245696003c880fc78ec56c
Delavalom <laarvelo@icloud.com>
2023-05-16 19:34:55 -0400

    adding experimental api

--- src/pages/api/payload.ts
+++ src/pages/api/payload.ts
@@ -11,42 +11,48 @@ type Data = {
   message: string;
 };
 
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   const event = req.headers["x-github-event"];
-  if (event === "push") {
-    const body = req.body as PushEvent;
-    const branchName = body.ref.split("/").pop() as string;
-    console.log(
-      `Received push event for repo ${body.repository.name} and branch ${branchName}`
-    );
+  try {
+    if (event === "push") {
+      const body = req.body as PushEvent;
+      const branchName = body.ref.split("/").pop() as string;
+      console.log(
+        `Received push event for repo ${body.repository.name} and branch ${branchName}`
+      );
 
-    const octokit = await octokitApp.getInstallationOctokit(
-      body.installation.id
-    );
+      const octokit = await octokitApp.getInstallationOctokit(
+        body.installation.id
+      );
 
-    await octokit.request(
-      "POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches",
-      {
-        owner: "Delavalom",
-        repo: "monoripify",
-        workflow_id: "build.yml",
-        ref: "main",
-        inputs: {
-          "app_id": env.APP_ID,
-          "webhook_secret_token": env.WEBHOOK_SECRET_TOKEN,
-          "private_key": env.PRIVATE_KEY
-        },
-        headers: {
-          "X-GitHub-Api-Version": "2022-11-28",
-        },
-      }
-    );
-
-    return res.status(200).json({ message: "succeed" });
+      await octokit.request(
+        "POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches",
+        {
+          owner: "Delavalom",
+          repo: "monoripify",
+          workflow_id: "build.yml",
+          ref: "main",
+          inputs: {
+            app_id: env.APP_ID,
+            webhook_secret_token: env.WEBHOOK_SECRET_TOKEN,
+            private_key: env.PRIVATE_KEY,
+          },
+          headers: {
+            "X-GitHub-Api-Version": "2022-11-28",
+          },
+        }
+      );
+      return res.status(200).json({ message: "succeed" });
+    }
+    return res.status(400).json({ message: "failed" });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error.message)
+    }
+    console.log(error)
+    return res.status(500).json({ message: "server error" })
   }
-  res.status(400).json({ message: "failed" });
 }
